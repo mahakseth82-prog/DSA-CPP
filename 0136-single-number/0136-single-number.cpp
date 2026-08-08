bfb8b97aa17a346d04b06e372f44dd79e2540aed
Mahak Seth <mahakseth82@gmail.com>
2026-08-08 23:03:37 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.32%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -4,7 +4,8 @@ public:
         int n=nums.size();
         int XOR=0;
         for(int i=0;i<n;i++){
-        XOR=XOR^nums[i];
+        XOR=XOR^nums[i]; // All duplicates thats are available will become zero after XOR with each other
+                         //only the single elment will be remain left ans thats will be ans;
         }
         return XOR;
     }
